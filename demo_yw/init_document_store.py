4220853425997f340b7117f5430c493bc6d8809a
yingzwang <yingzwang@hotmail.com>
2021-12-09 21:36:09 +0100

    Set split_length to 1000 in init_document_store.py.

--- demo_yw/init_document_store.py
+++ demo_yw/init_document_store.py
@@ -1,9 +1,7 @@
 import os
-from typing import Callable, Dict, List, Optional, Tuple, Union, Generator
 from haystack.document_stores.elasticsearch import ElasticsearchDocumentStore
 from haystack.utils.preprocessing import convert_files_to_dicts
 from haystack.nodes.preprocessor import PreProcessor
-from haystack.utils import launch_es
 from absl import app, flags, logging
 from absl.flags import FLAGS
 flags.DEFINE_string('doc_dir', './data/ziggo_wifi_en', 'Directory of documents (txt, pdf, docx)')
@@ -13,14 +11,14 @@ flags.DEFINE_string('doc_dir', './data/ziggo_wifi_en', 'Directory of documents (
 def load_and_clean_documents(doc_dir: str):
     # TODO: add function "add_period_to_end_of_sentence", add a period to the end of a bullet point sentence,
     #  e.g., for '1_smartwifi_pods.txt', add period to the end of sentences.
+    # Note: split_paragraphs=False (default) is good. Do not split paragraphs, better keep it a long document.
     raw_docs = convert_files_to_dicts(dir_path=doc_dir) # just read all text files, do not clean anything yet
     preprocessor = PreProcessor(
         clean_empty_lines=True,
         clean_whitespace=True,
         clean_header_footer=True,
         split_by="word",
-        split_length=100, # original demo
-        # split_length=1000, # haystack official demo
+        split_length=1000, # Note: keep it the same as the pipelines.yaml in rest_api
         split_respect_sentence_boundary=True,
     )
     docs = preprocessor.process(raw_docs)
